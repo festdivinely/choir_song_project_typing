a83a28d947bf2c3953f5b902dde788a6b03ab3ea
festdivinely <festusheav@gmail.com>
2025-05-19 04:39:20 -0400

    first commit

---
+++ constants/icons.ts
@@ -0,0 +1,41 @@
+import home from "@/assets/icons/home.png";
+import search from "@/assets/icons/search.png";
+import person from "@/assets/icons/person.png";
+import logo from "@/assets/icons/logo.png";
+import save from "@/assets/icons/save.png";
+import star from "@/assets/icons/star.png";
+import play from "@/assets/icons/play.png";
+import arrow from "@/assets/icons/arrow.png";
+import explore from "@/assets/icons/explore.png"
+import deleteicon from "@/assets/icons/deleteicon.png"
+import settings from "@/assets/icons/settings.png"
+import stop from "@/assets/icons/stop.png"
+import close from "@/assets/icons/close.png" 
+import open from "@/assets/icons/open.png" 
+import info from "@/assets/icons/info.png" 
+import pause from "@/assets/icons/pause.png" 
+import contactus from "@/assets/icons/contactus.png"
+import thoughts from "@/assets/icons/thoughts.png"
+import hamburger from "@/assets/icons/hamburger.png"
+
+export const icons = {
+  home,
+  search,
+  person,
+  logo,
+  save,
+  star,
+  play,
+  arrow,
+  explore,
+  deleteicon,
+  settings,
+  stop,
+  close,
+  open,
+  info,
+  pause,
+  contactus,
+  thoughts,
+  hamburger
+};
---
+++ constants/images.ts
@@ -0,0 +1,17 @@
+import background1 from "@/assets/images/background1.png";
+import background2 from "@/assets/images/background2.png";
+import bluishbg from "@/assets/images/bluishbg.png"
+import bg from "@/assets/images/bg.png";
+import chosenlogo from "@/assets/images/chosenlogo.png";
+import highlight from "@/assets/images/highlight.png";
+import rankingGradient from "@/assets/images/rankingGradient.png";
+
+export const images = {
+  bg,
+  highlight,
+  rankingGradient,
+  background1,
+  background2,
+  chosenlogo,
+  bluishbg
+};
