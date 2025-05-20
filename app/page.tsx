d9374eb48489be86ea740234c42f214ab8f30ed0
festdivinely <festusheav@gmail.com>
2025-05-19 22:52:43 -0400

    thirteenth  commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { images } from '../constants/images';
 import MenuBtns from "../components/menuBtns"
 import TypewriterText from "../components/typeWritter"
 import TypeAndEditBtn from "../components/typeAndEditBtn"
+import bluishbg from "../assets/images/bluishbg.png"
 
 export default function Home() {
   return (
     <main
       className="min-h-screen flex flex-col justify-between bg-cover bg-center text-white px-8 py-6"
-      style={{ backgroundImage: `url(${images.bluishbg.src})` }}
+      style={{ backgroundImage: `url(${bluishbg})` }}
     >
       {/* Top Section */}
       <MenuBtns />
