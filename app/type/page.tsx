0d1bdbbd2704e318c5bbac99f995d51e9b274754
festdivinely <festusheav@gmail.com>
2025-06-03 13:04:22 -0400

    twenty nine commit

--- app/type/page.tsx
+++ app/type/page.tsx
@@ -1,15 +1,21 @@
-import React from 'react'
+// app/type/page.tsx
 import { prisma } from '@/lib/prisma'
+import { Post } from '@prisma/client'
+import React from 'react'
 
-export default async function Type() {
-  const posts = await prisma.post.findMany({})
+export default async function TypePage() {
+  const posts: Post[] = await prisma.post.findMany()
 
   return (
     <div>
       {posts.length > 0 ? (
         posts.map((post) => (
           <div key={post.id}>
-            {post.title}
+            <h2>{post.title}</h2>
+            <p>{post.content}</p>
+            <p>
+              <em>{new Date(post.createdAt).toLocaleString()}</em>
+            </p>
           </div>
         ))
       ) : (
