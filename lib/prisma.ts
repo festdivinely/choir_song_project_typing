6182d158649323c59560095605c4cdc9e2612836
festdivinely <festusheav@gmail.com>
2026-02-18 13:37:46 -0500

    Fix Prisma structure for Vercel 5

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,19 +1,12 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const db =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.POSTGRES_PRISMA_URL,
-      },
-    },
-  });
+export const prisma =
+  globalForPrisma.prisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = db;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
 }
